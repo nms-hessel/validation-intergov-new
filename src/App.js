208edf2b86e3676b7430a2735dddd8c984a41eda
Hessel <hessel@newmediasquad.com>
2025-01-31 14:51:20 +0100

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import SIRValidationTool from "./SIRValidationTool";
+
+function App() {
+  return (
+    <div>
+      <SIRValidationTool />
+    </div>
+  );
+}
+
+export default App;
